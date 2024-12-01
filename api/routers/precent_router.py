a4fa47fb2ff780f15cdbb010d880940c150301ee
Данила Чуприн <danilacuprin@MacBook-Pro-Danila.local>
2024-12-01 17:51:29 +0300

    Added error handling for database inition

--- api/routers/precent_router.py
+++ api/routers/precent_router.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import pandas as pd
 import io
 
+
 class PrecentRouter(RegisterRoutes):
     prefix = "/precents"
     tags = ["precents"]
@@ -16,11 +17,17 @@ class PrecentRouter(RegisterRoutes):
     database: Database
 
     @classmethod
-    async def get_GetPrecent(cls, audience1: str | None = None, audience2: str | None = None):
+    async def get_GetPrecent(
+        cls, audience1: str | None = None, audience2: str | None = None
+    ):
         try:
-            result = await cls.database.get_precent(audience1=audience1, audience2=audience2)
+            result = await cls.database.get_precent(
+                audience1=audience1, audience2=audience2
+            )
         except sqlalchemy.exc.DBAPIError:
-            return {"error": "В первой группе нет респондентов либо указан неверный фильтр в audience"}
+            return {
+                "error": "В первой группе нет респондентов либо указан неверный фильтр в audience"
+            }
         except DatabaseException as e:
             return {"error": e.message}
         return result
@@ -31,7 +38,7 @@ class PrecentRouter(RegisterRoutes):
 
         def chunk_csv_pandas(file: bytes, chunk_size: int):
             """Reads a CSV file in chunks using pandas."""
-            reader = pd.read_csv(io.StringIO(contents.decode()), chunksize=chunk_size)
+            reader = pd.read_csv(io.StringIO(file.decode()), chunksize=chunk_size)
             for chunk in reader:
                 yield chunk
 
@@ -40,22 +47,26 @@ class PrecentRouter(RegisterRoutes):
         for chunk in chunk_csv_pandas(file=contents, chunk_size=100):
             object_to_create = []
             for data in chunk.to_numpy():
-                data_element = data[0].split(';')
+                data_element = data[0].split(";")
                 data_element[1] = datetime.strptime(data_element[1], "%Y%m%d").date()
                 object_to_create.append(
                     CreateRespondentModelSchema(
                         id=int(data_element[0]),
-                        date=data_element[1], 
-                        respondent=data_element[2], 
-                        sex=data_element[3], 
-                        age=data_element[4], 
-                        weight=data_element[5]
+                        date=data_element[1],
+                        respondent=data_element[2],
+                        sex=data_element[3],
+                        age=data_element[4],
+                        weight=data_element[5],
                     )
                 )
-            
+
             tasks.append(cls.database.insert_database_data(instances=object_to_create))
-        
-        await asyncio.gather(*tasks)
+        try:
+            await asyncio.gather(*tasks)
+        except Exception:
+            return {"error": "Что-то пошло не так"}
+
+        return {"success": True}
 
     @classmethod
     async def get_ping(cls):
